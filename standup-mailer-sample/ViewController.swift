f9dd534670b3f0ebdd721db720d485ca38cd7ad3
otukutun <orehaorz@gmail.com>
2016-11-30 14:17:58 +0900

    メーラー起動することを確認.

--- standup-mailer-sample/ViewController.swift
+++ standup-mailer-sample/ViewController.swift
@@ -32,6 +32,20 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, SFS
             present(brow, animated: true, completion: nil)
             return
         }
+        
+        // ref: http://qiita.com/kobaboy/items/60a2f44ad53675cfa0ae
+        let mailViewController = MFMailComposeViewController()
+        let toRecipients = ["to@example.com"] //Toのアドレス指定
+        let CcRecipients = ["cc@exaple.com","Cc2@exaple.com"] //Ccのアドレス指定
+        let BccRecipients = ["Bcc@1gmail.com","Bcc2@exaple.com"] //Bccのアドレス指定
+        
+        mailViewController.mailComposeDelegate = self
+        mailViewController.setSubject("メールの件名")
+        mailViewController.setToRecipients(toRecipients) //Toアドレスの表示
+        mailViewController.setCcRecipients(CcRecipients) //Ccアドレスの表示
+        mailViewController.setBccRecipients(BccRecipients) //Bccアドレスの表示
+        mailViewController.setMessageBody("メールの本文", isHTML: false)
+        self.present(mailViewController, animated: true, completion: nil)
     }
 
 }
